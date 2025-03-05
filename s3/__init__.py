dca2788f0552d455f95ec4f35e6d87a7df8c665d
Administrator <15270000+guiding-me@user.noreply.gitee.com>
2025-03-05 15:01:32 +0800

    阿里云上minio配置

--- s3/__init__.py
+++ s3/__init__.py
@@ -3,10 +3,17 @@ from minio import Minio
 
 bucket_name = "fusion-v1"
 
-# MinIO 服务器信息
+# MinIO 本地服务服务器信息
+# endpoint = "localhost:9000"  # 替换为你的 MinIO 服务器地址
+# access_key = "1crWtlpyNrtxfqGIMuDb"  # 替换为你的 Access Key
+# secret_key = "aB89qxRziGdKHYIwvSoQ9aZGqO3G6vv4LUv2bEe7"  # 替换为你的 Secret Key
+
+#阿里云服务器配置
 endpoint = "localhost:9000"  # 替换为你的 MinIO 服务器地址
-access_key = "1crWtlpyNrtxfqGIMuDb"  # 替换为你的 Access Key
-secret_key = "aB89qxRziGdKHYIwvSoQ9aZGqO3G6vv4LUv2bEe7"  # 替换为你的 Secret Key
+access_key = "nJuySBK9RNRkT6POJldP"  # 替换为你的 Access Key
+secret_key = "uQMShA9puTroYkdXnOwsktEmu7JTaYUwGozNjNSN"  # 替换为你的 Secret Key
+
+
 secure = False  # 如果使用 HTTPS，设置为 True；否则设置为 False
 
 # 初始化 MinIO 客户端
